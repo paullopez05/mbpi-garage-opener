ddf4869bb8791a2f33deff43db96bf4d710aabb8
mwyzlic <67292227+mwyzlic@users.noreply.github.com>
2020-06-22 17:19:02 -0700

    Create doorStatusViaAccelerometer.py
    Checks accelerometer.is_gesture() every half second to see if the door status changed.  If it did, report it via radio to the status receiver

---
+++ doorStatusViaAccelerometer.py
@@ -0,0 +1,28 @@
+from microbit import *
+import radio
+
+radio.config(group=13, data_rate=radio.RATE_250KBIT, power=7)
+
+doorOpen = False
+statusChanged = False
+
+while True:
+    # Check to see if micro:bit is either 'face down' or 'face up' 
+    # Check both to ensure door status is accurate with either side attached to the door
+    if accelerometer.is_gesture('face down') or accelerometer.is_gesture('face up'):
+        if not doorOpen:
+            doorOpen = True
+            statusChanged = True
+    else:
+        if doorOpen:
+            doorOpen = False
+            statusChanged = True
+    print(doorOpen)
+    
+    # Only send door status to Receiver if it changed 
+    if statusChanged:
+        radio.on()
+        radio.send('A')
+        radio.off()
+        statusChanged = False
+    sleep(500)
