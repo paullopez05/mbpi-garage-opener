cec488e8ba72e17751f6443c93f513933616238b
WyzardTech <67292227+WyzardTech@users.noreply.github.com>
2020-06-27 21:27:37 -0700

    Update GarageDoorLogger.py
    Added IFTTT power webrequest to get notifications on phone

--- GarageDoorLogger.py
+++ GarageDoorLogger.py
@@ -1,21 +1,25 @@
 import serial
 from datetime import datetime
+import requests
 
 ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
 ser.close()
 ser.open()
 
 log = open('log.txt', 'a')
+keyfile = open('IFTTT_key', 'r')
+IFTTT_key = keyfile.readline()
 
 while True:
     statusReceived = ser.readline()
     if str(statusReceived) != "b''":
         if str(statusReceived) == "b'True\\r\\n'":
-            garageDoorStatus = 'Garage Door Open'
+            garageDoorStatus = 'Open'
         elif str(statusReceived) == "b'False\\r\\n'":
-            garageDoorStatus = 'Garage Door Closed'
+            garageDoorStatus = 'Closed'
         else:
             garageDoorStatus = 'Unknown Status Received'
-        print(str(datetime.now()), garageDoorStatus)
-        log.write(str(datetime.now()) + ': ' + garageDoorStatus + '\n')
+        print(str(datetime.now()), 'Garage Door ' + garageDoorStatus)
+        log.write(str(datetime.now()) + ': Garage Door ' + garageDoorStatus + '\n')
+        requests.post('https://maker.ifttt.com/trigger/Garage_Door/with/key/' + IFTTT_key, params={"value1":garageDoorStatus,"value2":"none","value3":"none"})
         log.flush()
